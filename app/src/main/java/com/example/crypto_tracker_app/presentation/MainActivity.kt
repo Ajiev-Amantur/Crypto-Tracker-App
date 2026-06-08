bc4b4058a346d3b06237ef315fc37b275c607b46
amant <amanturumar78@gmail.com>
2026-06-08 14:45:43 +0600

    Added Search Tocen

--- app/src/main/java/com/example/crypto_tracker_app/presentation/MainActivity.kt
+++ app/src/main/java/com/example/crypto_tracker_app/presentation/MainActivity.kt
@@ -16,10 +16,15 @@ import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -61,6 +66,7 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -70,19 +76,42 @@ class MainActivity : ComponentActivity() {
                 NavHost(navController,"UI1") {
                 composable("UI1"){
                     val tocens by cryptoViewModel.tocen.observeAsState()
-                    LazyColumn(modifier = Modifier.fillMaxSize(
-                    ).statusBarsPadding())
-                    {
-                        items(items = tocens?: emptyList()){token->
-                            cryptoUI(token.name,token.current_price
-                                ,image = token.image,
-                                navController,
-                                cryptoViewModel,
-                                token
-                                )
 
+                    var searchText by remember {
+                        mutableStateOf("")
                     }
-                }
+                    Column(modifier = Modifier.fillMaxSize().statusBarsPadding()) {
+                        SearchBar(
+                            query = searchText,
+                            onQueryChange = { text ->
+                                searchText = text
+                            },
+                            onSearch = {
+
+                            },
+                            active = false,
+                            onActiveChange = {},
+                            placeholder = { Text("Search") }
+                        ) {
+
+                        LazyColumn(
+                            modifier = Modifier.fillMaxSize(
+                            ).statusBarsPadding())
+                        {
+                            val filtredTOcens = tocens?.filter {
+                                it.name.contains(searchText, ignoreCase = true)
+                            }
+                            items(items = filtredTOcens ?: emptyList()) { token ->
+                                cryptoUI(
+                                    token.name, token.current_price, image = token.image,
+                                    navController,
+                                    cryptoViewModel,
+                                    token
+                                )
+                            }
+                            }
+                        }
+                    }
                     }
                     composable("UI2"){
                         val selectedTocen by cryptoViewModel.selectedTocen.observeAsState()
@@ -145,7 +174,8 @@ NavHostController,viewModel: CryptoViewModel,tocen: CryptoTocensModel){
 //@Preview(showBackground = true)
 //@Composable
 //fun Preview(){
-//    cryptoUI("crypto",1222.0,"alla")
+//   cryptoUI("crypto",1222.0,"alla",
+//       NavHostController, CryptoViewModel, CryptoTocensModel)
 //}
 
 @Composable
